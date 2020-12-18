97908f9fe68417a0eb0d8bff0b87d90922aa769d
Adam Simpson <adam@heysparkbox.com>
2020-12-17 22:49:10 -0500

    feat: list command works

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use ddc_hi::Display;
-use argh::FromArgs;
-use mccs_db::ValueType;
 use anyhow::{anyhow, Result};
+use argh::FromArgs;
+use ddc_hi::Display;
+use mccs_db::ValueType;
 
 const INPUT_SELECT: u8 = 0x60;
 
@@ -27,27 +27,15 @@ enum Commands {
 }
 
 #[derive(FromArgs, Debug)]
-#[argh(
-    subcommand,
-    description = "list available monitors",
-    name = "list"
-)]
+#[argh(subcommand, description = "list available monitors", name = "list")]
 pub struct List {}
 
 #[derive(FromArgs, Debug)]
-#[argh(
-    subcommand,
-    description = "switch input on monitor.",
-    name = "switch"
-)]
+#[argh(subcommand, description = "switch input on monitor.", name = "switch")]
 pub struct Switch {
     #[argh(option, short = 'i', description = "input source to change too.")]
     input: String,
-    #[argh(
-        option,
-        short = 'm',
-        description = "the monitor to change inputs on.",
-    )]
+    #[argh(option, short = 'm', description = "the monitor to change inputs on.")]
     monitor: String,
 }
 
@@ -57,32 +45,45 @@ fn switch(monitor: String, input: String) -> Result<()> {
     //   let feature = display.info.mccs_database.get(INPUT_SELECT);
     //   // display.handle.set_vcp_feature(INPUT_SELECT, 0x12).expect("switched to HDMI2");
     // }
-  Ok(())
+    Ok(())
 }
 
 fn list() -> Result<()> {
-  for mut display in Display::enumerate() {
-    display.update_capabilities();
-    if let Some(d) = display.info.model_name {
-      println!("Monitor {} contains:", d);
-    }
-
-    if let Some(d) = display.info.mccs_database.get(INPUT_SELECT) {
-      if let ValueType::NonContinuous { interpretation: _, values } = &d.ty {
-          println!("{:?}", values);
+    println!("Listing displays...");
+    for mut display in Display::enumerate() {
+        println!("---Display---");
+        if let Err(err) = display.update_capabilities() {
+            println!("Error getting display capabilities: {}", err);
         }
-      }
+        if let Some(d) = display.info.model_name {
+            println!("{} has the following inputs:", d);
+        }
+
+        if let Some(d) = display.info.mccs_database.get(INPUT_SELECT) {
+            if let ValueType::NonContinuous {
+                interpretation: _,
+                values,
+            } = &d.ty
+            {
+                for input_types in values.values() {
+                    match input_types {
+                        Some(input) => println!("{:?}", input),
+                        None => (),
+                    }
+                }
+            }
+        }
     }
-  Ok(())
+    Ok(())
 }
 
 fn main() -> Result<()> {
-  let args: Args = argh::from_env();
+    let args: Args = argh::from_env();
 
-  match args.commands {
-    Commands::Switch(x) => switch(x.monitor, x.input)?,
-    Commands::List(_) => list()?,
-  }
+    match args.commands {
+        Commands::Switch(x) => switch(x.monitor, x.input)?,
+        Commands::List(_) => list()?,
+    }
 
-  Ok(())
+    Ok(())
 }
